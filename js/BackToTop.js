36c80d24bff8f61139cbd5d209cd7b996df67d00
SundaySmarty <202460108@stu.scls-sh.org>
2023-01-24 14:02:38 +0800

    update

--- js/BackToTop.js
+++ js/BackToTop.js
@@ -1,9 +1,33 @@
 window.onload = function() {
-    var backToTop = document.getElementById("back_to_top");
+    var cssStyleSheets = document.styleSheets;
+    function getStyle(cssName, selectorName) {
+        let cssTargetIndex, targetStyleIndex;
+        for(let i = 0; i < cssStyleSheets.length; i++) {
+            if(typeof cssStyleSheets[i].href == "string") {
+                if(cssStyleSheets[i].href.indexOf(cssName) >= 0)  cssTargetIndex = i;
+            }
+        }
+        for(let i = 0; i < cssStyleSheets[cssTargetIndex].rules.length; i++) {
+            if(typeof cssStyleSheets[cssTargetIndex].rules[i].selectorText == "string") {
+                if(cssStyleSheets[cssTargetIndex].rules[i].selectorText == selectorName) targetStyleIndex = i;
+            }
+        }
+        return [cssTargetIndex, targetStyleIndex];
+    }
+    var cssTargetIndex = getStyle("ChapterFormat.css", "#back_to_top")[0];
+    var targetStyleIndex = getStyle("ChapterFormat.css", "#back_to_top")[1];
+    var backToTop = cssStyleSheets[cssTargetIndex].rules[targetStyleIndex];
+    console.log(backToTop);
     window.onscroll = function() {
         var scrolltop = document.documentElement.scrollTop || document.body.scrollTop; //Get the scrollHeight of scrollbar
+        if(scrolltop < 500) {
+            backToTop.style.display = "none";
+        }
+        else {
+            backToTop.style.display = "";
+        }
     }
-    backToTop.onclick = function() {
+    document.getElementById("back_to_top").onclick = function() {
         document.documentElement.scrollTop = document.body.scrollTop = 0;
     }
 }
