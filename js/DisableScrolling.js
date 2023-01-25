221f1b6ba8b3e50c12d7e5731f194b88ca12aace
SundaySmarty <202460108@stu.scls-sh.org>
2023-01-25 13:42:29 +0800

    update

--- js/DisableScrolling.js
+++ js/DisableScrolling.js
@@ -1,10 +1,8 @@
-window.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener('touchstart', function(event) {
-        if(event.touches.length > 1) {
-            event.preventDefault();
-        }
-    })
-    document.addEventListener('gesturestart', function(event) {
+document.addEventListener('touchstart', function(event) {
+    if(event.touches.length > 1) {
         event.preventDefault();
-    })
+    }
+})
+document.addEventListener('gesturestart', function(event) {
+    event.preventDefault();
 })
