6756c39f5426d2412b60921f7596e84ba8665b8e
SundaySmarty <202460108@stu.scls-sh.org>
2023-03-20 17:18:23 +0800

    update

--- js/BackToTop.js
+++ js/BackToTop.js
@@ -7,16 +7,16 @@ window.addEventListener("DOMContentLoaded", function() {
                 if(cssStyleSheets[i].href.indexOf(cssName) >= 0)  cssTargetIndex = i;
             }
         }
-        for(let i = 0; i < cssStyleSheets[cssTargetIndex].rules.length; i++) {
-            if(typeof cssStyleSheets[cssTargetIndex].rules[i].selectorText === "string") {
-                if(cssStyleSheets[cssTargetIndex].rules[i].selectorText == selectorName) targetStyleIndex = i;
+        for(let i = 0; i < cssStyleSheets[cssTargetIndex].cssRules.length; i++) {
+            if(typeof cssStyleSheets[cssTargetIndex].cssRules[i].selectorText === "string") {
+                if(cssStyleSheets[cssTargetIndex].cssRules[i].selectorText == selectorName) targetStyleIndex = i;
             }
         }
         return [cssTargetIndex, targetStyleIndex];
     }
     var cssTargetIndex = getStyle("ChapterFormat.css", "#back_to_top")[0];
     var targetStyleIndex = getStyle("ChapterFormat.css", "#back_to_top")[1];
-    var backToTop = cssStyleSheets[cssTargetIndex].rules[targetStyleIndex];
+    var backToTop = cssStyleSheets[cssTargetIndex].cssRules[targetStyleIndex];
     window.onscroll = function() {
         var scrolltop = document.documentElement.scrollTop || document.body.scrollTop; //Get the scrollHeight of scrollbar
         if(scrolltop < 600) {
